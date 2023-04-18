18d3ad1842d0b78d825548a7eb2e99dee05f48f9
Xavier2023 <orjintaxavier@gmail.com>
2023-04-18 21:49:21 +0100

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,33 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Message from "./components/message";
-import Navbar from "./components/navbar";
-import HeroScreen from "./components/heroScreen";
-import Next from "./components/Next";
+// import Message from "./components/message";
+// import Navbar from "./components/navbar";
+// import HeroScreen from "./components/heroScreen";
+// import Next from "./components/Next";
+import LandingPage from "./pages/Landing-Page";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import ContactUs from "./forms/ContactUs";
 import RequestForm from "./forms/RequestForm";
 import ManufacturersForm from "./forms/ManufacturersForm";
 import SupplierForm from "./forms/SupplierForm";
-import Cards from "./components/Cards";
-import Why from "./components/Why";
-import Footer from "./components/Footer";
+// import Cards from "./components/Cards";
+// import Why from "./components/Why";
+// import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="app">
       <Router>
-        <Route exact path="/" component={Navbar}></Route>
+      <Route exact path="/" component={LandingPage}></Route>
+        {/* <Route exact path="/" component={Navbar}></Route>
         <Route exact path="/" component={Message}></Route>
         <Route exact path="/" component={HeroScreen}></Route>
         <Route exact path="/" component={Next}></Route>
+        <Route exact path="/" component={Cards}></Route>
+        <Route exact path="/" component={Why}></Route>
+        <Route exact path="/" component={Footer}></Route> */}
         <Switch>
           <Route exact path="/signup" component={Signup}></Route>
           <Route exact path="/login" component={Login}></Route>
@@ -36,12 +41,7 @@ function App() {
           <Route exact path="/contactus" component={ContactUs}></Route>
         </Switch>
       </Router>
-
-      <Navbar />
-      {/* <Message /> */}
-      <HeroScreen />
-      <Next />
-      <Cards />
+      {/* <Cards />
       <Why />
       <Footer />
       <Router>
@@ -58,7 +58,7 @@ function App() {
             component={ManufacturersForm}
           ></Route>
         </Switch>
-      </Router>
+      </Router> */}
     </div>
   );
 }
--- src/pages/Landing-Page.jsx
+++ src/pages/Landing-Page.jsx
@@ -1,17 +1,17 @@
 import React from "react";
-import Message from "./components/message";
-import Navbar from "./components/navbar";
-import HeroScreen from "./components/heroScreen";
-import Next from "./components/Next";
-import Cards from "./components/Cards";
-import Why from "./components/Why";
-import Footer from "./components/Footer";
+import Message from "../components/message";
+import Navbar from "../components/navbar";
+import HeroScreen from "../components/heroScreen";
+import Next from "../components/Next";
+import Cards from "../components/Cards";
+import Why from "../components/Why";
+import Footer from "../components/Footer";
 
 const LandingPage = () => {
   return (
     <div>
       <Navbar />
-      {/* <Message /> */}
+      <Message />
       <HeroScreen />
       <Next />
       <Cards />
