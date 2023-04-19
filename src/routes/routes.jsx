448f981e0cf8ced51ead51cf5dae760b39909525
Xavier2023 <orjintaxavier@gmail.com>
2023-04-19 11:41:21 +0100

    new

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import LandingPage from "../Pages/Landing-Page";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
@@ -6,10 +11,31 @@ import ContactUs from "../forms/ContactUs";
 import RequestForm from "../forms/RequestForm";
 import ManufacturersForm from "../forms/ManufacturersForm";
 import SupplierForm from "../forms/SupplierForm";
+import Root from "../pages/root";
 
+// const router = createBrowserRouter ([
+//   {
+//     path: "/",
+//     element: <Root />,
+//     children: [
+//       {
+//         path: "LandingPage",
+//         element: <LandingPage />,
+//       },
+//       {
+//         path: "Signup",
+//         element: <Signup />,
+//       },
+//       {
+//         path: "Login",
+//         element: <Login />,
+//       },
+//     ],
+//   },
+// ]);
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<AppLayout />}>
+    <Route path="/" element={<Root />}>
       <Route index element={<LandingPage />} />
       <Route path="/Signup" element={<Signup />} />
       <Route path="/Login" element={<Login />} />
@@ -17,9 +43,13 @@ const router = createBrowserRouter(
       <Route path="/RequestForm" element={<RequestForm />} />
       <Route path="/ManufacturesForm" element={<ManufacturersForm />} />
       <Route path="/SupplierForm" element={<SupplierForm />} />
-      <Route path="*" element={<NotFound />} />
+      {/* <Route path="*" element={<NotFound />} /> */}
     </Route>
   )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} fallbackElement={<BigSpinner />} />
 );
 
 export default router;
