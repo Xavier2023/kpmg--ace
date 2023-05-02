7aa1b6e84bcd06a702f34516605ea05914d24854
Jan1Verse1 <oluwaseyiadedotun@gmail.com>
2023-05-02 10:03:56 +0100

    ..

--- src/forms/ManufacturersForm.jsx
+++ src/forms/ManufacturersForm.jsx
@@ -8,67 +8,6 @@ const ManufacturersForm = () => {
       <div className="Manufacturers-section">
         <h1>Manufacturers Form</h1>
         <div className="Manufacturer-content">
-          {/* <form className="left-side">
-            <div className="form-group">
-              <label htmlFor="name">Firstname</label>
-              <input type="text" name="name" placeholder="Enter Firstname" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email Adderss</label>
-              <input
-                type="email"
-                name="email"
-                placeholder="Enter Email Address"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Company/Organization</label>
-              <input
-                type="text"
-                name="name"
-                placeholder="Enter Company/Organization "
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Select industry/Sector</label>
-              <select name="sector" id="industry">
-                <option value="select">Select</option>
-                <option value="Agricultural Sector">Agricultural Sector</option>
-                <option value="Industrial Sector">Industrial Sector</option>
-              </select>
-            </div>
-            <button className="btn">
-              <Link to="/signup">Pervious</Link>
-            </button>
-          </form>
-          <form className="right-side">
-            <div className="form-group">
-              <label htmlFor="name">Lastname</label>
-              <input type="text" name="name" placeholder="Enter Lastname " />
-            </div>
-            <div className="form-group">
-              <label htmlFor="phone">Phone Number</label>
-              <input
-                type="phone"
-                name="phone"
-                placeholder="Enter Phone Number "
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Role</label>
-              <input type="text" name="name" placeholder="Enter Role" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="name">Select your Location</label>
-              <select name="location" id="location">
-                <option value="select">Select</option>
-                <option value="Industrial Sector">Industrial Sector</option>
-              </select>
-            </div>
-            <button className="btn">
-              <Link to="/manufacturersportal">Proceed</Link>
-            </button>
-          </form> */}
           <form className="theForm" action="">
             <div class="formInputs">
               <div class="theInput">
